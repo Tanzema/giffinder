01a905eaf1f39957298f0a4b828f0bcf1934f7d4
Tanzema <tfazal@tywls.org>
2018-03-14 22:29:00 +0000

    dogs

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 $(document).ready(function(){
-  
-  
-  
-  
+    $.ajax({
+        url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=1",
+        method: "GET",
+        success: function(response) {
+            $('body').append('<img src=' + response.data[0].images.original.url + '>');
+        }
+    });
 });
