277f075f87969692d4a41351653ccd0c8df36e68
Tanzema <tfazal@tywls.org>
2018-04-18 21:23:20 +0000

    time to present

--- js/script.js
+++ js/script.js
@@ -4,10 +4,32 @@
 
 $(document).ready(function(){
     $.ajax({
-        url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=1",
+        url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=10",
         method: "GET",
         success: function(response) {
-            $('body').append('<img src=' + response.data[0].images.original.url + '>');
+            $('body').append('<img src=' + response.data[1].images.original.url + '>');
         }
+    });
+    
+});
+$(document).ready(function() {
+
+    console.log('the doc is ready bro');
+    function getGiphy(i) {
+        console.log('The getGiphy Function was called bro');
+        $.ajax({
+            url: "https://api.giphy.com/v1/stickers/search?q=" + i + "&api_key=dc6zaTOxFJmzC&limit=13",
+            method: "GET",
+            success: function(response) {
+                var image = response.data[0].images.original.url;
+                $('#gif').html('<img src=' + image + '>');
+            },
+        });
+    }
+    $("#search").click(function() {
+        console.log(' the button was clicked bro');
+        var input = $("#search-term").val;
+        console.log(input);
+        getGiphy(input);
     });
 });
